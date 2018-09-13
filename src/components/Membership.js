039712524e5f3dfb6530c54e6e121446b26d554f
Drageniix <drageniix@gmail.com>
2018-09-12 21:36:58 -0500

    Bolding and centering.

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -2,18 +2,20 @@ import React from 'react'
 
 const Membership = ({ data }) => (
     <section id="membership" className="membership">
-        <div className="membership__frequency">
-            <h2 className="section__title">
-                {data.meetingFrequency.prefix}
-            </h2>
-            <p className="membership__frequency--main">
-                <strong> 🏝️ {data.meetingFrequency.normal} </strong>
-            </p>
-            <p className="membership__frequency--exception">{data.meetingFrequency.exceptions}</p>
-        </div>
+        <div className="membership__container">
+            <div className="membership__frequency">
+                <h2 className="section__title">
+                    {data.meetingFrequency.prefix}
+                </h2>
+                <p className="membership__frequency--main">
+                    <strong> 🏝️ {data.meetingFrequency.normal} </strong>
+                </p>
+                <p className="membership__frequency--exception">{data.meetingFrequency.exceptions}</p>
+            </div>
 
-        <p className="membership__detail">{data.abstract}</p>
-        <p className="membership__detail">{data.details}</p>
+            <p className="membership__detail">{data.abstract}</p>
+            <p className="membership__detail">{data.details}</p>
+        </div>
     </section>
 )
 
